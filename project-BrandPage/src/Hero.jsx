1ef0ea0c5ffae1d6faa478ede0377e2a3e81f0fe
Anil <anilmende2002@email.com>
2025-08-27 00:04:08 +0530

    v3

--- project-BrandPage/src/Hero.jsx
+++ project-BrandPage/src/Hero.jsx
@@ -3,22 +3,24 @@ const HeroSection = () => {
         <main className="hero container">
             <div className="hero-content">
                 <h1>YOUR FEET DESERVE THE BEST</h1>
-                <p>YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP WITH OUR SHOES.</p>
-            </div>
+                <p>YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP WITH OUR SHOES. YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP WITH OUR SHOES.</p>
 
-            <div className="hero-buttons">
-                <button>Shop Now</button>
-                <button>Category</button>
-            </div>
-
-            <div className="shop-here">
-                <p>Also Available On</p>
-
-                <div className="app-stores">
-                    <img src="/Images/flipkart.png" alt="amazon" />
-                    <img src="/Images/amazon.png" alt="flipkart" />
+                <div className="hero-buttons">
+                    <button className="primary-btn">Shop Now</button>
+                    <button className="secondary-btn">Category</button>
                 </div>
+
+                <div className="shop-here">
+                    <p>Also Available On</p>
+
+                    <div className="online-stores">
+                        <img src="/Images/flipkart.png" alt="amazon" />
+                        <img src="/Images/amazon.png" alt="flipkart" />
+                    </div>
+                </div>
+
             </div>
+
 
 
             <div className="hero-image">
